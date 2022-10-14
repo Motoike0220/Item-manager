37af3c7fead8a8b5268a055ee89a3d967631afa8
Motoike0220 <104264544+Motoike0220@users.noreply.github.com>
2022-10-14 23:14:46 +0900

    heroku deploy

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->charaset('utf-8')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->tinyinteger('user_level')->default('0');
