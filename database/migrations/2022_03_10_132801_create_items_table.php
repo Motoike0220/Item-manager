46a795513ca09458bd37e14097567b21bc996f82
Motoike0220 <104264544+Motoike0220@users.noreply.github.com>
2022-10-15 22:02:46 +0900

    DB修正

--- database/migrations/2022_03_10_132801_create_items_table.php
+++ database/migrations/2022_03_10_132801_create_items_table.php
@@ -18,7 +18,7 @@ class CreateItemsTable extends Migration
             $table->bigInteger('user_id')->unsigned()->index();
             $table->string('user_name',100)->charset("utf8")->index();
             $table->string('name', 100)->charset("utf8")->index();
-            $table->smallInteger('status')->default('0');
+            $table->smallInteger('status')->default('1');
             $table->smallInteger('type')->nullable();
             $table->string('detail', 500)->charset("utf8")->nullable();
             $table->timestamps();
